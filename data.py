12bdb7c532883e8947e14f07ff6c36e86ed34c99
Zyad Yasser <zyad.yasser@nyu.edu>
2023-05-17 17:35:58 +0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,42 @@
+[
+    [
+        "&#x1f331",
+        "Sustainable Agriculture Courses",
+        "The Introduction to Sustainable Agriculture course teaches primary school students in Kakuma about environmentally-friendly farming practices.",
+        "The course covers topics such as soil health, water conservation, and organic techniques.",
+        "Through interactive activities, students gain a deeper understanding of sustainability and the natural world.",
+        "The course empowers students to make positive impacts on the environment and their communities.",
+        "assets/img/agriculture/01.jpg",
+        "0"
+    ],
+    [
+        "&#x1F467;&#x1F466",
+        "Children Emotional Support",
+        "Analyze the emotional needs of children in a camp setting to provide targeted support and intervention as needed.",
+        "Teach strategies and techniques for emotional regulation, coping with stress, and building resilience to help children navigate difficult situations.",
+        "Create a supportive and inclusive environment where children feel safe to express their emotions and receive validation and guidance from trained professionals.",
+        "Integrate emotional support and learning opportunities into the club's activities, such as through team-building exercises, art therapy, or mindfulness practices.",
+        "assets/img/children/01.jpg",
+        "1"
+    ],
+    [
+        "&#x1f9fc",
+        "Soap Production and Distribution",
+        "Teach the chemistry and mechanics of soap making, including the selection of ingredients and the use of equipment and safety protocols.",
+        "Demonstrate various techniques for molding, scenting, and coloring soap, and encourage experimentation and creativity in individual soap making projects.",
+        "Guide club members in developing a business plan for soap production and distribution, including market research, branding, and pricing strategies.",
+        "Provide opportunities for club members to sell their soap products, such as through a pop-up shop, online marketplace, or partnership with a local retailer.",
+        "assets/img/soap/01.jpg",
+        "2"
+    ],
+    [
+        "&#x1f41f;&#x1f986;&#x1f997",
+        "Animal Farming and Fishing in the Desert",
+        "Teach sustainable and responsible animal husbandry practices that are appropriate for the unique challenges of desert farming, such as water conservation and heat management.",
+        "Provide hands-on experience in raising fish, turkeys, rabbits, ducks, and other animals commonly raised in desert farming operations.",
+        "Demonstrate how the integration of animal farming and fishing can improve soil quality and enhance crop yields, creating a more sustainable and profitable farming system.",
+        "Guide club members in developing a business plan for animal farming and fishing in the desert, including market research, product pricing, and distribution strategies.",
+        "assets/img/farming/01.jpg",
+        "3"
+    ],
+]
